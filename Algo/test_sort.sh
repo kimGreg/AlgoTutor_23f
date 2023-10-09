dfc05f3698c62e16ebaead5b963e9d8e226537dd
Jaeyun Kim <jaeyunkim@snu.ac.kr>
2023-10-09 21:00:23 +0900

    fix timeout and print time

--- Algo/test_sort.sh
+++ Algo/test_sort.sh
@@ -4,22 +4,22 @@
 echo -e "test bubble sort\n============================"
 
 echo -n "[sort_integer_small] : "
-python3 ../testcase/test.py --path ../testcase/sort_integer_small --cmd "./test_algo bubble_sort" --timeout 2
+python3 ../testcase/test.py --path ../testcase/sort_integer_small --cmd ./test_algo --arg bubble_sort --timeout 0.2
 
 echo -e "\ntest insert sort\n============================"
 
 echo -n "[sort_integer_small] : "
-python3 ../testcase/test.py --path ../testcase/sort_integer_small --cmd "./test_algo insert_sort" --timeout 2
+python3 ../testcase/test.py --path ../testcase/sort_integer_small --cmd ./test_algo --arg insert_sort --timeout 1
 
 echo -e "\ntest merge sort\n============================"
 
 echo -n "[sort_integer_small] : "
-python3 ../testcase/test.py --path ../testcase/sort_integer_small --cmd "./test_algo merge_sort" --timeout 2
+python3 ../testcase/test.py --path ../testcase/sort_integer_small --cmd ./test_algo --arg merge_sort --timeout 1
 echo -n "[sort_integer_large] : "
-python3 ../testcase/test.py --path ../testcase/sort_integer_large --cmd "./test_algo merge_sort" --timeout 3
+python3 ../testcase/test.py --path ../testcase/sort_integer_large --cmd ./test_algo --arg merge_sort --timeout 2
 
 
 echo -e "\ntest shell sort\n============================"
 
 echo -n "[sort_integer_small] : "
-python3 ../testcase/test.py --path ../testcase/sort_integer_small --cmd "./test_algo shell_sort" --timeout 2
+python3 ../testcase/test.py --path ../testcase/sort_integer_small --cmd ./test_algo  --arg shell_sort --timeout 1
--- testcase/test.py
+++ testcase/test.py
@@ -2,16 +2,43 @@
 
 from multiprocessing import Pool
 import os
-import subprocess
+import subprocess, psutil
 import argparse
+import time
+
 
 class Testcase:
   def run_test(self, tc_number):
     input_path = os.path.join(self.inputs_path, tc_number)
+    
+    timeStarted = time.time()   
 
-    test = subprocess.run(f"{self.run_cmd} < {input_path}", shell=True, stdout=subprocess.PIPE, timeout=self.timeout)
+    # parent=subprocess.Popen(f"{self.run_cmd} {self.run_arg} < {input_path}", shell=False, stdout=subprocess.PIPE)
+    # for _ in range(self.timeout*10): # sleep for 0.1 second
+    #     if parent.poll() is not None:  # process just ended
+    #       break
+    #     time.sleep(0.1)
+    # else:
+    #   # the for loop ended without break: timeout
+    #   parent = psutil.Process(parent.pid)
+    #   for child in parent.children(recursive=True):  # or parent.children() for recursive=False
+    #       child.kill()
+    #   parent.kill()
+    
+    # (res, err) = parent.communicate()
+    
+    try:
+      input = open(input_path, 'rb')
+      content = input.read()
+      test = subprocess.run([self.run_cmd, self.run_arg], input=content , shell=False, stdout=subprocess.PIPE, timeout=self.timeout)
+    except subprocess.TimeoutExpired:
+      return "", self.timeout
+
     result = test.stdout.decode("utf-8").strip()
-    return result
+
+    timeDelta = time.time()-timeStarted
+
+    return result, timeDelta
   
   @staticmethod
   def get_content(file_path):
@@ -29,6 +56,7 @@ class Testcase:
   def __init__(self, args):
     self.tc_path = args.tc_path
     self.run_cmd = args.cmd
+    self.run_arg = args.cmd_arg
     self.timeout = args.timeout
     self.inputs_path = os.path.join(self.tc_path, "in")
     self.outputs_path = os.path.join(self.tc_path, "out")
@@ -37,10 +65,10 @@ class Testcase:
 
   def work(self, tc_name):
     answer = self.get_content(os.path.join(self.outputs_path, tc_name))
-    result = self.run_test(tc_name)
+    result, time = self.run_test(tc_name)
     if result == answer:
-      return 1
-    return 0
+      return 1, time
+    return 0, time
 
   def run_testcase(self):
     pool=Pool()
@@ -49,19 +77,33 @@ class Testcase:
     for i in range(1, self.tc_num+1):
       result.append(pool.apply_async(self.work, args=(str(i),)))
     
-    suc = 0
+    tot_suc = 0
+    tot_time = 0
+    cnt = 0
+    is_timeout = False
     for res in result:
-      suc += res.get()
+      suc, time = res.get()
+      tot_suc += suc
+      tot_time += time
+      cnt += 1
+
+      if(time == self.timeout):
+        is_timeout = True
     
     pool.close()
     pool.join()
- 
-    print(f"{suc}/{self.tc_num}")
+    
+    if is_timeout:
+      print(f"{tot_suc}/{cnt}, (timeout)")
+    else:
+
+      print(f"{tot_suc}/{cnt}, {int(tot_time/cnt*10000)/10} ms")
 
 parser = argparse.ArgumentParser('test case')
 parser.add_argument('--path', dest='tc_path', help='testcase base directory', type=str)
 parser.add_argument('--cmd', dest='cmd', help='rum command', type=str)
-parser.add_argument('--timeout', dest='timeout', help='timeout', type=int, default=2)
+parser.add_argument('--arg', dest='cmd_arg', help='rum command argument', type=str)
+parser.add_argument('--timeout', dest='timeout', help='timeout', type=float, default=1)
 args = parser.parse_args()
 
 if __name__ == "__main__":
